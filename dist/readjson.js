1305e2b45020c6cd983f3fb3072f55498fa7b415
JolaWalach <joanna.walach@op.pl>
2021-12-15 23:22:43 +0100

    links added

--- dist/readjson.js
+++ dist/readjson.js
@@ -4,7 +4,7 @@ const linkMonthly = document.getElementById("paramonthly");
 const hours = document.getElementsByClassName("print-data");
 const lastWeek = document.getElementsByClassName("lastweek");
 
-function getDailyDates(link) {
+linkDaily.addEventListener("click", function getDailyDates() {
     for (let i = 0; i < hours.length; i++) {
         fetch('./dist/data.json')
             .then((response) => {
@@ -15,63 +15,43 @@ function getDailyDates(link) {
                 hours[i].textContent = data[i].timeframes.daily.current + ' hrs';
                 lastWeek[i].textContent = data[i].timeframes.daily.previous + ' hrs';
             })
-
             .catch((err) => {
                 console.log(err)
-                // Do something for an error here
             })
     }
-}
+})
 
-function getWeeklyDates(link) {
+
+linkWeekly.addEventListener("click", function getWeeklyDates() {
     for (let i = 0; i < hours.length; i++) {
         fetch('./dist/data.json')
             .then((response) => {
                 return response.json()
             })
             .then((data) => {
-                console.log(i);
                 hours[i].textContent = data[i].timeframes.weekly.current + ' hrs';
                 lastWeek[i].textContent = data[i].timeframes.weekly.previous + ' hrs';
             })
 
             .catch((err) => {
                 console.log(err)
-                // Do something for an error here
             })
     }
-}
+})
 
-function getMonthlyDates(link) {
+linkMonthly.addEventListener("click", function getMonthlyDates() {
     for (let i = 0; i < hours.length; i++) {
         fetch('./dist/data.json')
             .then((response) => {
                 return response.json()
             })
             .then((data) => {
-                console.log(i);
                 hours[i].textContent = data[i].timeframes.monthly.current + ' hrs';
                 lastWeek[i].textContent = data[i].timeframes.monthly.previous + ' hrs';
             })
 
             .catch((err) => {
                 console.log(err)
-                // Do something for an error here
             })
     }
-}
-
-function switchDates(link) {
-if (link == "daily") {
-linkDaily.addEventListener("click", getDailyDates("daily"));
-console.log("daily");
-} else if (link == "weekly") {
-linkWeekly.addEventListener("click", getWeeklyDates("weekly"));
-console.log("weekly");
-} else if (link == "monthly") {
-linkMonthly.addEventListener("click", getMonthlyDates("monthly"));
-console.log("monthly");
-} 
-}
-
-switchDates("monthly");
+})
